570ba9da5703d72bdcacae80d07945f33a0005b0
gimsong3 <shkim9783@gmail.com>
2023-04-28 16:57:46 +0900

    20230428 StyleSheet, props study

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-
 import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greeting';
+import Box from './components/Box';
 
 const App = () => {
-  const name = 'JSX';
-
   return (
     <SafeAreaView>
       {/* 주석을 작성해봅시다. */}
-      <Greeting
-        name={name} //이름을 설정하기
-      />
+
+      {/* <Box /> */}
+      <Box rounded size="large" color="pink" />
+      {/* <Box rounded={true} /> 기본형 */}
     </SafeAreaView>
   );
 };
--- LearnReactNative/components/Box.js
+++ LearnReactNative/components/Box.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-function Box() {
-  return <View style={styles.box} />;
+/*
+function Box(props) {
+  // return <View style={[styles.box, styles.rounded]} />; // 배열을 사용하여 적용할 스타일을 적용하는 듯
+
+  // props가 true 일 때만 스타일 부여
+  // return <View style={[styles.box, props.rounded ? styles.rounded : null]} />;
+  return (
+    <View
+      style={[
+        styles.box,
+        props.rounded && styles.rounded,
+        sizes[props.size],
+        {
+          backgroundColor: props.color,
+        },
+      ]}
+    />
+  ); // 혹은 이렇게도 작성 가능
 }
+*/
+
+//==== 객체 구조 분해 할당 사용 =====//
+function Box({rounded, size, color}) {
+  return (
+    <View
+      style={[
+        styles.box,
+        rounded && styles.rounded,
+        sizes[size],
+        {backgroundColor: color},
+      ]}
+    />
+  );
+}
+
+Box.defaultProps = {
+  size: 'medium',
+  // rounded: true,
+  color: 'black',
+};
 
 const styles = StyleSheet.create({
   box: {
@@ -11,6 +48,27 @@ const styles = StyleSheet.create({
     height: 64,
     backgroundColor: 'black',
   },
+  rounded: {
+    borderRadius: 16,
+  },
+  small: {
+    width: 32,
+    height: 32,
+  },
+  medium: {
+    width: 64,
+    height: 64,
+  },
+  large: {
+    width: 128,
+    height: 128,
+  },
 });
+
+const sizes = {
+  small: styles.small,
+  medium: styles.medium,
+  large: styles.large,
+};
 
 export default Box;
